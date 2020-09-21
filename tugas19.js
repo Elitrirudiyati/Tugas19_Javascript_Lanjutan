000a93df1a7ab906655bd6e2b20dfb172d54b72e
Elitrirudiyati <71365611+Elitrirudiyati@users.noreply.github.com>
2020-09-21 11:43:29 +0700

    Add files via upload

---
+++ tugas19.js
@@ -0,0 +1,24 @@
+
+var ulang = 'Semangat...'
+var hasil2 = ulang.repeat(5)
+console.log(hasil2)
+
+var ulang = '*'
+var hasil2 = ulang.repeat(5)
+console.log(hasil2)
+var hasil2 = ulang.repeat(25)
+console.log(hasil2)
+var hasil2 = ulang.repeat(50)
+console.log(hasil2)
+var hasil2 = ulang.repeat(75)
+console.log(hasil2)
+var hasil2 = ulang.repeat(100)
+console.log(hasil2)
+var hasil2 = ulang.repeat(75)
+console.log(hasil2)
+var hasil2 = ulang.repeat(50)
+console.log(hasil2)
+var hasil2 = ulang.repeat(25)
+console.log(hasil2)
+var hasil2 = ulang.repeat(5)
+console.log(hasil2)
